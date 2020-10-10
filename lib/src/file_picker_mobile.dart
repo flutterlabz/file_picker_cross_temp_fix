bf43e7ab2cdb0426aabe3910a1fa6b6a34316ca6
TheOneWithTheBraid <the-one@with-the-braid.cf>
2020-10-10 08:59:05 +0200

    Fixed incompatibility to dependencies

--- lib/src/file_picker_mobile.dart
+++ lib/src/file_picker_mobile.dart
@@ -9,21 +9,24 @@ import 'file_picker_io.dart';
 /// Implementation of file selection dialog using file_picker for mobile platforms
 Future<Map<String, Uint8List>> selectFilesMobile(
     {FileTypeCross type, String fileExtension}) async {
-  File file = await FilePicker.getFile(
-      type: fileTypeCrossParse(type),
-      allowedExtensions: parseExtension(fileExtension));
-  return {file.path: file.readAsBytesSync()};
+  PlatformFile file = (await FilePicker.platform.pickFiles(
+          type: fileTypeCrossParse(type),
+          allowedExtensions: parseExtension(fileExtension)))
+      .files[0];
+  return {file.path: file.bytes};
 }
 
 /// Implementation of file selection dialog for multiple files using file_picker for mobile platforms
 Future<Map<String, Uint8List>> selectMultipleFilesMobile(
     {FileTypeCross type, String fileExtension}) async {
-  List<File> files = await FilePicker.getMultiFile(
-      type: fileTypeCrossParse(type),
-      allowedExtensions: parseExtension(fileExtension));
+  List<PlatformFile> files = (await FilePicker.platform.pickFiles(
+          type: fileTypeCrossParse(type),
+          allowMultiple: true,
+          allowedExtensions: parseExtension(fileExtension)))
+      .files;
   Map<String, Uint8List> filesMap = {};
   files.forEach((file) {
-    filesMap[file.path] = file.readAsBytesSync();
+    filesMap[file.path] = file.bytes;
   });
   return filesMap;
 }
