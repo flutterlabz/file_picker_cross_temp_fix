dfa07cd90175ddffded7ac33e497abd9dfcf7ffb
CDDelta <ngjunyen@gmail.com>
2020-12-03 01:16:58 +0000

    Update lib/src/file_picker_web.dart

--- lib/src/file_picker_web.dart
+++ lib/src/file_picker_web.dart
@@ -22,12 +22,10 @@ Future<Map<String, Uint8List>> selectSingleFileAsBytes(
     final reader = new html.FileReader();
 
     reader.onLoadEnd.listen((e) {
-      loadEnded.complete({
-        uploadInput.value.replaceAll('\\', '/'):
-            Base64Decoder().convert(reader.result.toString().split(",").last)
-      });
+      loadEnded
+          .complete({uploadInput.value.replaceAll('\\', '/'): reader.result});
     });
-    reader.readAsDataUrl(file);
+    reader.readAsArrayBuffer(file);
   });
   return loadEnded.future;
 }
@@ -54,12 +52,11 @@ Future<Map<String, Uint8List>> selectMultipleFilesAsBytes(
       final reader = new html.FileReader();
       reader.onLoadEnd.listen((e) {
         fileBytes[(currentFile.relativePath + '/' + currentFile.name)
-                .replaceAll('\\', '/')] =
-            Base64Decoder().convert(reader.result.toString().split(",").last);
+            .replaceAll('\\', '/')] = reader.result;
         counter++;
         if (counter >= files.length) loadEnded.complete(fileBytes);
       });
-      reader.readAsDataUrl(currentFile);
+      reader.readAsArrayBuffer(currentFile);
     });
   });
   return loadEnded.future;
@@ -90,9 +87,10 @@ Future<bool> saveInternalBytes({Uint8List bytes, String path}) async {
 /// Dummy implementation throwing an error. Should be overwritten by conditional imports.
 Future<String> exportToExternalStorage(
     {Uint8List bytes, String fileName}) async {
-  html.AnchorElement link =
-      html.AnchorElement(href: Uri.dataFromBytes(bytes).toString())
-        ..download = fileName;
+  html.AnchorElement link = html.AnchorElement(
+      href: html.Url.createObjectUrlFromBlob(
+          html.Blob([bytes], 'application/octet-stream')))
+    ..download = fileName;
   link.click();
   return (fileName);
 }
